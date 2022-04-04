cb169ddb49167788efe668da9b30d7edc8395872
aluno <aluno@DSN1251017044>
2022-04-04 10:35:28 -0300

    Primeiro Commit

---
+++ OrientacaoaObjeto/src/Calculadora.java
@@ -0,0 +1,26 @@
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		double n1 = 5; 
+		double n2 = 2;
+		double resulS;
+		double resulSU;
+		double resulM;
+		double resulD;
+		
+		resulS = n1 + n2;
+		System.out.println("O resultado da soma é: " + resulS);
+		
+		resulSU = n1 - n2;
+		System.out.println("O resultado da subtração é: " + resulSU);
+		
+		resulM = n1 * n2;
+		System.out.println("O resultado da multiplicação é: " + resulM);
+		
+		resulD = n1 / n2;
+		System.out.println("O resultado da divisão é: " + resulD);
+
+	}
+
+}
