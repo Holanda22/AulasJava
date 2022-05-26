d858aa959435ec0f1617d5e8859c3bfa83a7c04b
aluno <aluno@DSN1251017049>
2022-05-26 11:14:26 -0300

    commit

---
+++ ExercicioTxt/src/Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException, FileNotFoundException {
+		
+//		Scanner teclado = new Scanner(System.in);
+//		String texto;
+//		try (BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in))){
+//			while()
+		}
+//		try (BufferedWriter escrever = new BufferedWriter(new FileWriter("Texto.txt"))){
+//			System.out.println("Digite seu texto:");
+//			escrever.write(texto);
+//			
+////			while(teclado.hasNext()) {
+////			
+////			}
+////			teclado.close();
+//		
+//		
+//		try (BufferedReader reader = new BufferedReader(new FileReader("Texto.txt"))) {
+//			String line;
+//			String string = "";
+//
+//			while ((line = reader.readLine()) != null) {
+//				string += line + "\n";
+//			}
+//			System.out.println(string);
+//		}
+		
+
+	}
+
+}
