8e2594e3a6139829ab27628e1818a1dab3789ce2
aluno <aluno@DSN1251017049>
2022-05-26 09:11:13 -0300

    List para txt

--- EscreverTxt/src/Main.java
+++ EscreverTxt/src/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -13,6 +15,24 @@ public class Main {
 																							// apagado para não se criar
 																							// arquivos corrompidos
 			escrever.write(s);
+		}
+
+		Mouse mouse1 = new Mouse(false, 2, "pequeno");
+		Mouse mouse2 = new Mouse(true, 5, "grande");
+		Mouse mouse3 = new Mouse(true, 3, "médio");
+
+		List<Mouse> mouses = new ArrayList<Mouse>();
+		
+		mouses.add(mouse1);
+		mouses.add(mouse2);
+		mouses.add(mouse3);
+
+		try (BufferedWriter escrever2 = new BufferedWriter(new FileWriter("Mouses.txt"))) {
+			for (Mouse mouse : mouses) { // fazendo o for de interação com a List dentro do BufferedWriter se utiliza a
+											// porta aberta para escrever a quantidade de vezes que for necessário de
+											// acordo com a quantidade de elementos da List
+				escrever2.write(mouse.toString());
+			}
 		}
 
 	}
