cb41ddd1d7788068edef5d2d0e4ac711a13d4916
Aluno <Aluno@DSN1251210704>
2022-07-21 11:18:01 -0300

    temperatura

--- LogicaDeProgramação/src/main.java
+++ LogicaDeProgramação/src/main.java
@@ -6,31 +6,31 @@ public class main {
 		
 		Scanner entrada = new Scanner(System.in);
 		
-//		System.out.println("Digite a temperatura em graus Celsius:");
-//		double C = entrada.nextInt();
+		System.out.println("Digite a temperatura em graus Celsius:");
+		double C = entrada.nextInt();
+		
+		double F, K, Re, Ra;
+		F = C*1.8 + 32;
+		K = C+273.15;
+		Re = C*0.8;
+		Ra = C + 1.8 + 32 + 459.67;
+		
+		System.out.println("Temperatura nas escalas:");
+		System.out.println("Fahrenheit: " + F);
+		System.out.println("Rï¿½aumur: " + Re);
+		System.out.println("Kelvin: " + K);
+		System.out.println("Rankine: " + Ra);
+		
+		
+//		System.out.println("Digite os minutos:");
 //		
-//		double F, K, Re, Ra;
-//		F = C*1.8 + 32;
-//		K = C+273.15;
-//		Re = C*0.8;
-//		Ra = C + 1.8 + 32 + 459.67;
+//		int min = entrada.nextInt();
 //		
-//		System.out.println("Temperatura nas escalas:");
-//		System.out.println("Fahrenheit: " + F);
-//		System.out.println("Réaumur: " + Re);
-//		System.out.println("Kelvin: " + K);
-//		System.out.println("Rankine: " + Ra);
+//		int anos = min / 525600;
+//		int dias = (min%525600)/1440;
 //		
-		
-		System.out.println("Digite os minutos:");
-		
-		int min = entrada.nextInt();
-		
-		int anos = min / 525600;
-		int dias = (min%525600)/1440;
-		
-		System.out.println(min + " minutos são aproximadamente " + anos + " ano(s) e " + dias + "dia(s).");
-		
+//		System.out.println(min + " minutos sï¿½o aproximadamente " + anos + " ano(s) e " + dias + "dia(s).");
+//		
 		
 
 	}
